4d77a089a29a913da1cd8f69541470ea5a0b1dab
Kyle Lu <kyle.lu@miife.com>
2013-06-04 11:21:07 -0400

    add readme

---
+++ README.md
@@ -0,0 +1,6 @@
+#Backbone check tree with requireJs
+## Introduction
+This is a simple piece of code to demostrate how to use backbone view to generate the check tree layout.
+
+## License
+Licensed under the [MIT license](LICENSE-MIT "MIT License"). 
--- js/test/specs/spec.js
+++ js/test/specs/spec.js
@@ -49,44 +49,24 @@ define(["jquery", "backbone", "views/Tree","collections/Collection", "jasminejqu
 		describe("Tree render", function() {
 		    // Runs before every Collection spec
 		    beforeEach(function() {
-			var collection = new Collection(),me = this,result;
-			collection.fetch({
-				success : function(data){
-					me.view = new View({data:data.toJSON()});
-					me.view.render();
-					result = true;
-				},
-				error : function () {
-					result = false;
-				}
-			})
-			waitsFor(function() {
-				return result === true;
-			}, "to retrive all nodes from json", 3000);
+			var json = [{"id":"node-id","title":"node 1","name":"node_1","expand":true,"children":[{"id":"leaf-1","title":"leaf 1","name":"leaf_1"},{"id":"leaf-2","title":"leaf 2","name":"leaf_2"},{"id":"leaf-3","title":"leaf 3","name":"leaf_3"}]}];
+			this.view = new View({data:json});
+			this.view.render();
 		    });
 
 		    it("should contain the non checked items", function() {
-			var me = this;
-			runs(function(){
-				expect(me.view.getCheckedItems().length).toEqual(0);
-			})
+			expect(this.view.getCheckedItems().length).toEqual(0);
 		    });
 
 		    it("should contain four checkbox input fields", function() {
-			var me = this;
-			runs(function(){
-				expect(me.view.$el.find('input[type="checkbox"]').length).toEqual(4);
-			})
+			expect(this.view.$el.find('input[type="checkbox"]').length).toEqual(4);
 		    });
 
 		    it("first node's ul tag should contain show class and span tag should contain expand class", function() {
-			var me = this;
-			runs(function(){
-				var ul = $(me.view.$el.find('ul'))
-				, span = ul.prev('span');
-				expect(ul.hasClass('show')).toBeTruthy();
-				expect(span.hasClass('expand')).toBeTruthy();
-			})
+			var ul = $(this.view.$el.find('ul'))
+			, span = ul.prev('span');
+			expect(ul.hasClass('show')).toBeTruthy();
+			expect(span.hasClass('expand')).toBeTruthy();
 		    });
 
 		}); // End of the Tree render test suite
